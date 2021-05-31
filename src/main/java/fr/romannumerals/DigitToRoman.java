2e2ae86661c55c9ee5686781516f070fc4f730db
Nathan PODESTA <nathan.podesta@icloud.com>
2021-05-31 23:19:38 +0200

    algo roman to digit and digit to roman

--- src/main/java/fr/romannumerals/DigitToRoman.java
+++ src/main/java/fr/romannumerals/DigitToRoman.java
@@ -1,2 +1,23 @@
-package fr.romannumerals;public class DigitToRoman {
+package fr.romannumerals;
+
+import java.util.Hashtable;
+
+public class DigitToRoman {
+    public static String toRoman(int digit) {
+        int digits[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String roman[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+        StringBuilder result = new StringBuilder();
+        int i = 0;
+        /* Les romains n'utilisaient pas le zéro, ils ne le considéraient
+           pas comme un chiffre mais comme un état de vide et donc ne l'écrivaient pas. */
+        do {
+            while ((digit - digits[i]) >= 0) {
+                result.append(roman[i]);
+                digit = digit - digits[i];
+            }
+            i++;
+        } while (digit > 0);
+        return result.toString();
+    }
 }
--- src/main/java/fr/romannumerals/RomanToDigits.java
+++ src/main/java/fr/romannumerals/RomanToDigits.java
@@ -1,8 +1,38 @@
 package fr.romannumerals;
 
+import java.util.Hashtable;
+
 public class RomanToDigits {
-    public int toDigits(String roman) {
-        System.out.println(roman);
-        return 0;
+    private static Hashtable<Character, Integer> values = new Hashtable<Character, Integer>();
+    static {
+        values.put('I', 1);
+        values.put('V', 5);
+        values.put('X', 10);
+        values.put('L', 50);
+        values.put('C', 100);
+        values.put('D', 500);
+        values.put('M', 1000);
+    }
+
+    public static int toDigits(String roman) {
+        int result = 0;
+        int i = 0;
+        int len = roman.length();
+        for (; i < len; i++) {
+            int v1 = values.get(roman.charAt(i));
+            if (i + 1 == len) {
+                result += v1;
+            } else {
+                int v2 = values.get(roman.charAt(i + 1));
+                if (v1 < v2) {
+                    result += v2;
+                    result -= v1;
+                    i++;
+                } else {
+                    result += v1;
+                }
+            }
+        }
+        return result;
     }
 }
